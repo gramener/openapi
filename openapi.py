4eb9d2ab2db855c7af6ebe1c479a16ac7360ccfa
S Anand <root.node@gmail.com>
2021-08-30 12:09:58 +0530

    WIP

--- openapi.py
+++ openapi.py
@@ -1,3 +1,4 @@
+from tornado.web import HTTPError
 import gramex
 import inspect
 import json
@@ -6,9 +7,37 @@ import re
 from typing import List, get_type_hints
 from typing_extensions import Annotated
 from textwrap import dedent
+from gramex.config import merge
 from gramex.transforms import handler, Header
 from gramex.transforms.transforms import typelist
 from gramex.handlers import BaseHandler
+
+error_codes = {
+    '200': {
+        'description': 'Successful Response',
+        'content': {'application/json': {}}
+    },
+    '400': {
+        'description': 'Bad request',
+        'content': {'text/html': {'example': 'Bad request'}}
+    },
+    '401': {
+        'description': 'Not authorized',
+        'content': {'text/html': {'example': 'Not authorized'}}
+    },
+    '403': {
+        'description': 'Forbidden',
+        'content': {'text/html': {'example': 'Forbidden'}}
+    },
+    '404': {
+        'description': 'Not found',
+        'content': {'text/html': {'example': 'Not found'}}
+    },
+    '500': {
+        'description': 'Internal server error',
+        'content': {'text/html': {'example': 'Internal server error'}}
+    },
+}
 
 
 def url_name(pattern):
@@ -39,7 +68,7 @@ class OpenAPI(BaseHandler):
             hint = hints.get(name, None)
             typ, is_list = typelist(hints[name]) if hint else (str, False)
             config = {
-                'in': 'header' if hint and getattr(hint, '_name', None) == 'Header' else 'query',
+                'in': 'header' if hint and hint is Header else 'query',
                 'name': name,
                 'description': getattr(param.annotation, '__metadata__', ('',))[0],
                 'schema': {}
@@ -57,18 +86,7 @@ class OpenAPI(BaseHandler):
                 config['schema']['items'] = {'type': cls.types.get(typ, 'string')}
             else:
                 config['schema']['type'] = cls.types.get(typ, 'string'),
-        # TODO: Automate error responses
-        spec['responses'] = {
-            '200': {
-                'description': 'Successful Response',
-                'content': {
-                    'application/json': {'schema': {}}
-                }
-            },
-            '404': {
-                'description': 'Not found'
-            },
-        }
+        spec['responses'] = error_codes
         return spec
 
     def get(self):
@@ -100,16 +118,17 @@ class OpenAPI(BaseHandler):
                     'summary': f'{url_name(pattern)}: {config["handler"]}'
                 },
             }
+            cls = gramex.service.url[key].handler_class
             if config['handler'] == 'FunctionHandler':
-                cls = gramex.service.url[key].handler_class
                 function = cls.info['function']
                 fnspec = self.function_spec(function.__func__ or function)
                 fnspec['summary'] = f'{url_name(pattern)}: {config["handler"]}'
                 default_methods = 'GET POST PUT DELETE PATCH OPTIONS'.split()
                 for method in getattr(cls, '_http_methods', default_methods):
                     info[method.lower()] = fnspec
+            # User's spec definition overrides our spec definition
+            merge(info, cls.conf.get('openapi', {}), mode='overwrite')
 
-        # TODO: Deep merge the defaults.
         self.write(json.dumps(spec))
 
 
@@ -126,8 +145,12 @@ def test_function(li1: List[int],
                   s1: str = 'Total',
                   n1: np.int = 0,
                   n2: np.int64 = 0,
-                  h: Header[str] = ''):
+                  h: Header = '',
+                  code: int = 200):
     '''
     This is a **Markdown** docstring.
     '''
-    return json.dumps([li1, li2, li3, lf1, lf2, lf3, l1, i1, i2, s1, h])
+    if code == 200:
+        return json.dumps([li1, li2, li3, lf1, lf2, lf3, l1, i1, i2, s1, h])
+    else:
+        raise HTTPError(code, reason='Something')
